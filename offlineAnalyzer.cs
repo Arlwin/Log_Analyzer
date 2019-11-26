ee301e6625a2a97aa505923e0035255569f50f48
Arlwin Fajardo (TS-PH) <arlwinkarlfajardo@gmail.com>
2019-11-26 16:25:07 +0800

    offlineAnalyzer ver 1

--- offlineAnalyzer.cs
+++ offlineAnalyzer.cs
@@ -3,20 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Log_Analyzer
 {
     class offlineAnalyzer
     {
-        String ofcdebugPath;
+        public List<String[]> errorsFound { get; set; }
 
-        public offlineAnalyzer(String path)
+        public offlineAnalyzer(String filepath, String codepath)
         {
-            ofcdebugPath = path;
-
+            analyze(filepath, codepath);
         }
 
+        //Main function
+        private void analyze(string filepath, string codepath)
+        {
+            //Load the file first
+            StreamReader f = new StreamReader(filepath);
 
+            List<string[]> errorCodes = loadCodes(codepath);
+
+            foreach (var errorCode in errorCodes)
+            {
+                checkError(f, errorCode);
+                f.BaseStream.Seek(0, SeekOrigin.Begin);
+            }
+        }
+
+        //Load the codes
+        private List<string[]> loadCodes(string codepath)
+        {
+            StreamReader code = new StreamReader(codepath);
+            List<string[]> codeList = new List<string[]>();
+
+            string line;
+            int counter = 0;
+
+            while ((line = code.ReadLine()) != null)
+            {
+                codeList[counter] = line.Split(',');
+                counter++;
+            }
+
+            return codeList;
+        }
+
+        //Search the file for errors inside the codes
+        private bool checkError(StreamReader r, string[] errorLine)
+        {
+            string line;
+
+            while ((line = r.ReadLine()) != null)
+            {
+                if (line.Contains(errorLine[0]))
+                {
+                    return true;
+                }               
+            }
+
+            return false;
+        }
 
     }
 }
